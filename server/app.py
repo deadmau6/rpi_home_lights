545a95b449c1c3e212abf23ecf35996f1d7866ba
deadmau6 <joepop16@gmail.com>
2020-03-01 14:01:34 -0800

    have socket respond correctly

--- server/app.py
+++ server/app.py
@@ -7,11 +7,7 @@ socketio = SocketIO(app, message_queue='redis://')
 
 @socketio.on('lights')
 def handle_lights(json):
-	emit(json, broadcast=True)
-
-@socketio.on('status')
-def current_status(json):
-	emit(json, broadcast=True)
+	emit('status', json, broadcast=True)
 
 if __name__ == '__main__':
 	socketio.run(app)
