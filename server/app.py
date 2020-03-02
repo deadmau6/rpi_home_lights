724fb34b72ee7735c1e60ac2d7c0cf18089efa94
deadmau6 <joepop16@gmail.com>
2020-03-01 17:41:58 -0800

    configure socket server

--- server/app.py
+++ server/app.py
@@ -1,13 +1,18 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'pi_boi'
-socketio = SocketIO(app, message_queue='redis://')
+socketio = SocketIO(app, cors_allowed_origins='*', message_queue='redis://')
 
 @socketio.on('lights')
 def handle_lights(json):
 	emit('status', json, broadcast=True)
 
 if __name__ == '__main__':
-	socketio.run(app)
+	socketio.run(app, debug=True, host='0.0.0.0', port=4000)
+
