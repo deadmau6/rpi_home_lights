163052113b0d2c30d8b8883a1da8886ed8085602
deadmau6 <joepop16@gmail.com>
2020-03-19 11:01:02 -0700

    Server that runs lights

---
+++ server/run_lights.py
@@ -0,0 +1,49 @@
+from multiprocessing import freeze_support
+from .lights import LightsManager
+from time import sleep
+import random
+import redis
+import pickle
+
+"""
+Event object format:
+{
+    id: number,
+    status: string,
+    mode: string,
+    mode_params: dict,
+}
+"""
+
+current_ID = random.randint(0, 10000)
+
+lights = LightsManager()
+
+lights.start_event({ 'id': current_ID, 'status': 'running', 'mode': 'SINGLE' })
+
+print("Started Event: {0}".format({ 'id': current_ID, 'status': 'running', 'mode': 'SINGLE' }))
+
+def handle_lights(json):
+    packet = pickle.loads(json['data'])
+    if packet['manager']:
+    	lights.write_event(packet['data'], current_ID)
+
+if __name__ == "__main__":
+	freeze_support()
+	r = redis.Redis(host='localhost', port=6379, db=0)
+    p = r.pubsub()
+    p.subscribe(**{'client': handle_lights})
+    p.get_message()
+    while True:
+        try:
+            p.get_message()
+            lights.monitor()
+            sleep(0.01)
+        except KeyboardInterrupt:
+            print('\nClosing Chat')
+            break
+        except Exception as e:
+            print("\nClosing from error: {0}".format(e))
+            break
+    p.unsubscribe()
+    p.close()
