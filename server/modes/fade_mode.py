b926a1e483bfdb1bbc89bdef9f2b9464843a88da
deadmau6 <joepop16@gmail.com>
2020-06-13 20:07:58 -0700

    Add Utils and Added a fade light mode

---
+++ server/modes/fade_mode.py
@@ -0,0 +1,79 @@
+from .lights_controller import LightsController
+from .utils import Utils
+from cerberus import Validator
+from time import sleep
+
+class FadeMode(LightsController):
+    _validator = Validator({
+            'cycle': {
+                'type': 'bool',
+                'coerce': Utils.string_to_bool
+            },
+            'num_of_colors': {
+                'type': 'integer',
+                'coerce': int
+            },
+            'steps': {
+                'type': 'integer',
+                'coerce': int,
+                'min': 0,
+                'max': 10,
+            },
+            'red': {
+                'type': 'integer',
+                'coerce': int,
+                'min': 0,
+                'max': 255,
+            },
+            'green': {
+                'type': 'integer',
+                'coerce': int,
+                'min': 0,
+                'max': 255,
+            },
+            'blue': {
+                'type': 'integer',
+                'coerce': int,
+                'min': 0,
+                'max': 255,
+            },
+        })
+
+    _current_index = 0
+
+    @property
+    def colors(self):
+        return getattr(self, '_colors', [[255,0,0], [0,255,0], [0,0,255]])
+
+    @setter.colors
+    def colors(self, colors):
+        if isinstance(colors, list):
+            # maybe check r + g + b >= 255
+            self._colors = colors
+        else:
+            # assume colors is a num
+            self._colors = [Utils.hsv_to_rgb(i, 1, 1) for i in range(0, 360, int(360/colors))]
+
+    def process(self, cycle=True, num_of_colors=3, steps=3, red=0, green=0, blue=0):
+        if cycle and len(self.colors) != num_of_colors:
+            self.colors = num_of_colors
+        else:
+            # maybe check r + g + b >= 255
+            self.colors = [red, green, blue]
+
+        step_size = [int(x / steps) for x in self.colors[self._current_index]]
+        current_color = [0, 0, 0]
+        # fade in
+        for i in range(0, steps):
+            current_color = [(current_color[i] + step_size[i]) % 255 for i in range(3)]
+            self.pixels.fill(current_color)
+            self.pixels.show()
+            sleep(1 / steps)
+        # fade out
+        for i in range(0, steps):
+            current_color = [abs(current_color[i] - step_size[i]) % 255 for i in range(3)]
+            self.pixels.fill(current_color)
+            self.pixels.show()
+            sleep(1 / steps)
+
+        self._current_index = self._current_index + 1 % len(self.colors)
---
+++ server/modes/uitls.py
@@ -0,0 +1,29 @@
+
+class Utils:
+
+    @staticmethod
+    def string_to_bool(s):
+        return s == 'True' or s =='true'
+
+    @staticmethod
+    def rgb_prime(h, c, x):
+        if 0 <= h < 60:
+            return c, x, 0
+        if 60 <= h < 120:
+            return x, c, 0
+        if 120 <= h < 180:
+            return 0, c, x
+        if 180 <= h < 240:
+            return 0, x, c
+        if 240 <= h < 300:
+            return x, 0, c
+        return c, 0, x
+
+    @staticmethod
+    def hsv_to_rgb(h, s, v):
+        # when 0 <= h < 360, 0 <= s <= 1, 0 <= v <= 1 
+        c = v * s
+        x = c * (1 - abs(h/60 % 2 - 1))
+        m = v - c
+        r, g, b = Utils.rgb_prime(h, c, x)
+        return (int((r + m) * 255), int((g + m) * 255), int((b + m) * 255))
